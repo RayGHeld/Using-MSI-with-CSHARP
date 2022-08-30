859ad7720bd764e003ed3983c1c74bfc97b72279
Ray Held <raheld@microsoft.com>
2022-08-30 17:38:30 -0500

    Updated code

--- CSHARP_MSI/Program.cs
+++ CSHARP_MSI/Program.cs
@@ -14,9 +14,9 @@ namespace CSHARP_MSI
 {
     internal class Program
     {
-        static string userAssignedClientId = "e16791{redacted}49d141978"; //client id for the user assigned MSI
-        static string keyVaultSecretName = "Rays-{redacted}Secret"; //name for the keyvault secret
-        static Uri keyVaultUri = new UriBuilder("https://rays-{redacted}.vault.azure.net/").Uri;
+        static string userAssignedClientId = "{client id}"; //"e167919d-4e6a-4149-96e2-90c49d141978"; //client id for the user assigned MSI
+        static string keyVaultSecretName = "{keyvault secret name}"; //name for the keyvault secret
+        static Uri keyVaultUri = new UriBuilder("https://rays-keyvault.vault.azure.net/").Uri;
 
         static void Main(string[] args)
         {
@@ -26,11 +26,9 @@ namespace CSHARP_MSI
 
                 Get_AccessToken_With_UserAssigned_MSI();
                 Get_Secret_With_UserAssigned_MSI();
-                Make_GraphRequest_With_UserMSI_Token();
 
                 Get_AccessToken_With_SystemAssigned_MSI();
                 Get_Secret_With_SystemAssigned_MSI();
-                Make_GraphRequest_With_SystemMSI_Token();
 
                 Console.WriteLine("Press Enter to try again or any other key to exit");
                 ConsoleKeyInfo key = Console.ReadKey();
@@ -49,9 +47,14 @@ namespace CSHARP_MSI
             ManagedIdentityCredential credential = new ManagedIdentityCredential(userAssignedClientId);
 
             SecretClient client = new SecretClient(keyVaultUri, credential);
-            KeyVaultSecret secret = client.GetSecret(keyVaultSecretName).Value;
-
-            Console.WriteLine($"KeyVault Secret = {secret.Value}\n");
+            try
+            {
+                KeyVaultSecret secret = client.GetSecret(keyVaultSecretName).Value;
+                Console.WriteLine($"KeyVault Secret = {secret.Value}\n");
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting secret: {ex.Message}");
+            }           
         }
 
         static void Get_Secret_With_SystemAssigned_MSI()
@@ -61,9 +64,15 @@ namespace CSHARP_MSI
             ManagedIdentityCredential credentail = new ManagedIdentityCredential();
 
             SecretClient client = new SecretClient(keyVaultUri, credentail);
-            KeyVaultSecret secret = client.GetSecret(keyVaultSecretName).Value;
-
-            Console.WriteLine($"KeyVault secret = {secret.Value}\n");
+            try
+            {
+                KeyVaultSecret secret = client.GetSecret(keyVaultSecretName).Value;
+                Console.WriteLine($"KeyVault Secret = {secret.Value}\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error getting secret: {ex.Message}");
+            }
         }
 
         static void Get_AccessToken_With_UserAssigned_MSI()
@@ -72,7 +81,7 @@ namespace CSHARP_MSI
 
             ManagedIdentityCredential credential = new ManagedIdentityCredential(userAssignedClientId);
 
-            AccessToken at = credential.GetToken(new TokenRequestContext(new string[] { "https://graph.microsoft.com" }));
+            AccessToken at = credential.GetToken(new TokenRequestContext(new string[] { "https://database.windows.net" }));
             string accessToken = at.Token;
 
             Console.WriteLine($"Access Token = {accessToken}");
@@ -85,51 +94,12 @@ namespace CSHARP_MSI
 
             ManagedIdentityCredential credentail = new ManagedIdentityCredential();
 
-            AccessToken at = credentail.GetToken(new TokenRequestContext(new string[] { "https://graph.microsoft.com" }));
+            AccessToken at = credentail.GetToken(new TokenRequestContext(new string[] { "https://database.windows.net" }));
             string accessToken = at.Token;
 
             Console.WriteLine($"Access token = {accessToken}");
 
         }
 
-        static void Make_GraphRequest_With_UserMSI_Token()
-        {
-            Console.WriteLine($"Making graph request with User MSI Token:");
-
-            ManagedIdentityCredential credential = new ManagedIdentityCredential(userAssignedClientId);
-            GraphServiceClient graphClient = new GraphServiceClient(credential);
-
-            IGraphServiceUsersCollectionPage users;
-
-            try
-            {
-                users = graphClient.Users.Request().GetAsync().Result;
-                Console.WriteLine($"Number of users in tenant: {users.Count}\n");
-            } catch(Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
-            }
-
-        }
-
-        static void Make_GraphRequest_With_SystemMSI_Token()
-        {
-            Console.WriteLine($"Making graph request with system MSI Token:");
-
-            ManagedIdentityCredential credential = new ManagedIdentityCredential();
-            GraphServiceClient graphClient = new GraphServiceClient(credential);
-
-            IGraphServiceUsersCollectionPage users;
-
-            try
-            {
-                users = graphClient.Users.Request().GetAsync().Result;
-                Console.WriteLine($"Number of users in tenant: {users.Count}\n");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
-            }
-        }
     }
 }
